843ac84ff16da3be5d3e06d8a8661098836328bd
Jordan Owens <jkowens@gmail.com>
2013-05-26 23:18:33 -0400

    Clean up Worker code formatting

--- app/code/community/Jowens/JobQueue/Model/Worker.php
+++ app/code/community/Jowens/JobQueue/Model/Worker.php
@@ -6,93 +6,93 @@ require_once('DJJob.php');
 
 class Jowens_JobQueue_Model_Worker extends Mage_Core_Model_Abstract 
 {
-	const DEFAULT_QUEUE = 'default';
+    const DEFAULT_QUEUE = 'default';
 
-	private $workerName;
-	private $queue;
-
-	public function __construct() {
-		list($hostname, $pid) = array(trim(`hostname`), getmypid());
+    private $workerName;
+    private $queue;
+            
+    public function __construct() {
+        list($hostname, $pid) = array(trim(`hostname`), getmypid());
         $this->workerName = "host::$hostname pid::$pid";
         $this->queue = Mage::getStoreConfig('jobqueue/config/queue');
         if(empty($this->queue)) {
-        	$this->queue = self::DEFAULT_QUEUE;	
+            $this->queue = self::DEFAULT_QUEUE; 
         }
-	}
+    }
 
-	public function getQueue() {
-		return $this->queue;
-	}
+    public function getQueue() {
+        return $this->queue;
+    }
 
-	public function setQueue($queue) {
-		$this->queue = $queue;
-	}
+    public function setQueue($queue) {
+        $this->queue = $queue;
+    }
 
-	public function getWorkerName() {
-		return $this->workerName;
-	}
+    public function getWorkerName() {
+        return $this->workerName;
+    }
 
-	
-	public function executeJobs($schedule=null) {
-		if(!Mage::getStoreConfig('jobqueue/config/enabled')) {
-			return;
-		}
+    
+    public function executeJobs($schedule=null) {
+        if(!Mage::getStoreConfig('jobqueue/config/enabled')) {
+            return;
+        }
 
-		if($schedule) {
-			$jobsRoot = Mage::getConfig()->getNode('crontab/jobs');
-	    	$jobConfig = $jobsRoot->{$schedule->getJobCode()};
-	    	$queue = $jobConfig->queue;
-	    	if($queue) {
-				$this->setQueue($queue);
-	    	}
-		}
+        if($schedule) {
+            $jobsRoot = Mage::getConfig()->getNode('crontab/jobs');
+            $jobConfig = $jobsRoot->{$schedule->getJobCode()};
+            $queue = $jobConfig->queue;
+            if($queue) {
+                $this->setQueue($queue);
+            }
+        }
 
-		$this->setupDJJob();
+        $this->setupDJJob();
 
-		try {
-			$collection = Mage::getModel('jobqueue/job')->getCollection();
-			$collection->addFieldToFilter('queue', array('eq' => $this->getQueue()))
-				->addFieldToFilter('run_at', array(
-		                array('null' => true),
-		                array('lteq'=> date('Y-m-d H:i:s', Mage::app()->getLocale()->storeTimeStamp()))					
-				))
-				->addFieldToFilter(array('locked_at', 'locked_by'), array(
-		                array('locked_at', 'null' => true),
-		                array('locked_by', 'eq' => $this->workerName)				
-				))				
-				->addFieldToFilter('failed_at', array('null' => true))
-				->addFieldToFilter('attempts', array('lt' => (int)Mage::getStoreConfig('jobqueue/config/max_attempts')));
+        try {
+            $collection = Mage::getModel('jobqueue/job')->getCollection();
+            $collection->addFieldToFilter('queue', array('eq' => $this->getQueue()))
+            ->addFieldToFilter('run_at', array(
+                array('null' => true),
+                array('lteq'=> date('Y-m-d H:i:s', Mage::app()->getLocale()->storeTimeStamp()))                 
+                ))
+            ->addFieldToFilter(array('locked_at', 'locked_by'), array(
+                array('locked_at', 'null' => true),
+                array('locked_by', 'eq' => $this->workerName)               
+                ))              
+            ->addFieldToFilter('failed_at', array('null' => true))
+            ->addFieldToFilter('attempts', array('lt' => (int)Mage::getStoreConfig('jobqueue/config/max_attempts')));
 
-			// randomly order to prevent lock contention among workers
-			$collection->getSelect()->order(new Zend_Db_Expr('RAND()'));
-			$collection->load();
+            // randomly order to prevent lock contention among workers
+            $collection->getSelect()->order(new Zend_Db_Expr('RAND()'));
+            $collection->load();
 
-			foreach($collection as $row) {
-	            $job = new DJJob($this->workerName, $row->getId(), array(
-	                "max_attempts" => Mage::getStoreConfig('jobqueue/config/max_attempts')
-	            ));
-	            if ($job->acquireLock()) {
-	            	$job->run();
-	            }
-			}
-		} catch (Exception $e) {
-			Mage::log($e);
-		}
-	}
+            foreach($collection as $row) {
+                $job = new DJJob($this->workerName, $row->getId(), array(
+                    "max_attempts" => Mage::getStoreConfig('jobqueue/config/max_attempts')
+                    ));
+                if ($job->acquireLock()) {
+                    $job->run();
+                }
+            }
+        } catch (Exception $e) {
+            Mage::log($e);
+        }
+    }
 
-	protected function setupDJJob() {
-		$config  = Mage::getConfig()->getResourceConnectionConfig("default_setup");
-		
-		$dsn = "";
-		if (strpos($config->host, '/') !== false) {
-			$dsn = "mysql:unix_socket=" . $config->host . ";dbname=" . $config->dbname;
-		} else {
-			$dsn = "mysql:host=" . $config->host . ";dbname=" . $config->dbname . ";port=" . $config->port;
-		} 
+    protected function setupDJJob() {
+        $config  = Mage::getConfig()->getResourceConnectionConfig("default_setup");
+        
+        $dsn = "";
+        if (strpos($config->host, '/') !== false) {
+            $dsn = "mysql:unix_socket=" . $config->host . ";dbname=" . $config->dbname;
+        } else {
+            $dsn = "mysql:host=" . $config->host . ";dbname=" . $config->dbname . ";port=" . $config->port;
+        } 
 
-		DJJob::configure(
-			$dsn, 
-			array('mysql_user' => $config->username, 'mysql_pass' => $config->password)
-		);
-	}
+        DJJob::configure(
+            $dsn, 
+            array('mysql_user' => $config->username, 'mysql_pass' => $config->password)
+            );
+    }
 }
